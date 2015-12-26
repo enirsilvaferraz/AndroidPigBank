b13b66466d6ed090d2b7b0c61cb302056b589dd6
enirsilvaferraz <enirsilvaferraz@gmail.com>
2015-12-26 19:57:37 -0200

    Chart

--- app/src/main/java/com/system/androidpigbank/controllers/activities/TransactionDetailActivity.java
+++ app/src/main/java/com/system/androidpigbank/controllers/activities/TransactionDetailActivity.java
@@ -1,25 +1,38 @@
 package com.system.androidpigbank.controllers.activities;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.View;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.system.androidpigbank.R;
 import com.system.androidpigbank.controllers.managers.CategoryManager;
 import com.system.androidpigbank.controllers.managers.LoaderResult;
 import com.system.androidpigbank.helpers.Constants;
 import com.system.androidpigbank.models.entities.Category;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 
 public class TransactionDetailActivity extends BaseActivity<List<Category>> {
@@ -36,11 +49,11 @@ public class TransactionDetailActivity extends BaseActivity<List<Category>> {
         setSupportActionBar(toolbar);
 
         container = findViewById(R.id.transaction_detail_container);
-        chart = (BarChart) findViewById(R.id.chart);
+        //chart = (BarChart) findViewById(R.id.chart);
 
         month = getIntent().getIntExtra("MONTH", 0);
 
-        getSupportLoaderManager().initLoader(Constants.LOADER_CATEGORY, null, this);
+        getSupportLoaderManager().restartLoader(Constants.LOADER_CATEGORY, null, this);
     }
 
     private BarDataSet getData(BarEntry object, String x) {
@@ -59,23 +72,20 @@ public class TransactionDetailActivity extends BaseActivity<List<Category>> {
 
         if (data.isSuccess()) {
 
-//            final int[] intArray = getResources().getIntArray(R.array.material_colors);
-            final int[] intArray = {R.color.material_blue, R.color.material_red, R.color.material_light_green};
-            Arrays.sort(intArray);
+            PieChart mChart = (PieChart) findViewById(R.id.chart);
+            mChart.setUsePercentValues(true);
+            mChart.setDescription("");
+            mChart.setRotationEnabled(true);
 
-            Calendar cal = Calendar.getInstance();
-            cal.set(Calendar.MONTH, month);
+            setData(data.getData(), 100, mChart);
 
-            BarData barData = new BarData();
-            barData.addXValue(new SimpleDateFormat("MMMM").format(cal.getTime()));
+            mChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
 
-            for (Category category : data.getData()) {
-                final BarDataSet dataSet = getData(new BarEntry(category.getAmount(), 0), category.getName());
-                dataSet.setColors(intArray);
-                barData.addDataSet(dataSet);
-            }
-
-            chart.setData(barData);
+            Legend legend = mChart.getLegend();
+            legend.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+            legend.setXEntrySpace(7f);
+            legend.setYEntrySpace(0f);
+            legend.setYOffset(0f);
 
         } else {
             chart.setVisibility(View.GONE);
@@ -86,5 +96,68 @@ public class TransactionDetailActivity extends BaseActivity<List<Category>> {
     @Override
     public void onLoaderReset(Loader<LoaderResult<List<Category>>> loader) {
 
+    }
+
+    private SpannableString generateCenterSpannableText() {
+
+        SpannableString s = new SpannableString("MPAndroidChart\ndeveloped by Philipp Jahoda");
+        s.setSpan(new RelativeSizeSpan(1.7f), 0, 14, 0);
+        s.setSpan(new StyleSpan(Typeface.NORMAL), 14, s.length() - 15, 0);
+        s.setSpan(new ForegroundColorSpan(Color.GRAY), 14, s.length() - 15, 0);
+        s.setSpan(new RelativeSizeSpan(.8f), 14, s.length() - 15, 0);
+        s.setSpan(new StyleSpan(Typeface.ITALIC), s.length() - 14, s.length(), 0);
+        s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length() - 14, s.length(), 0);
+        return s;
+    }
+
+    private void setData(List<Category> categories, float range, Chart mChart) {
+
+        ArrayList<Entry> yVals1 = new ArrayList<>();
+        ArrayList<String> xVals = new ArrayList<>();
+
+        for (int i = 0; i < categories.size(); i++) {
+            yVals1.add(new Entry(categories.get(i).getAmount(), i));
+            xVals.add(categories.get(i).getName());
+        }
+
+        PieDataSet dataSet = new PieDataSet(yVals1, "");
+        dataSet.setSliceSpace(2f);
+        dataSet.setSelectionShift(5f);
+
+        dataSet.setColors(getColors());
+
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.BLACK);
+
+        mChart.setData(data);
+
+        mChart.highlightValues(null);
+        mChart.invalidate();
+    }
+
+    @NonNull
+    private ArrayList<Integer> getColors() {
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        return colors;
     }
 }

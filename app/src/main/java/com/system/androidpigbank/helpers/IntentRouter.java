92e89c19fa7216e95db2b3eae3a943c2ffbca297
Enir Francisco Silva Ferraz <eferraz@ciandt.com>
2016-08-07 21:13:21 -0300

    Remoção das transações fixas

--- app/src/main/java/com/system/androidpigbank/helpers/IntentRouter.java
+++ app/src/main/java/com/system/androidpigbank/helpers/IntentRouter.java
@@ -5,13 +5,10 @@ import android.content.Intent;
 
 import com.system.androidpigbank.controllers.activities.CategoryListActivity;
 import com.system.androidpigbank.controllers.activities.CategoryManagerActivity;
-import com.system.androidpigbank.controllers.activities.FixedTransactionListActivity;
-import com.system.androidpigbank.controllers.activities.FixedTransactionManagerActivity;
 import com.system.androidpigbank.controllers.activities.TransactionManagerActivity;
 import com.system.androidpigbank.helpers.constant.Constants;
 import com.system.androidpigbank.models.business.BackupService;
 import com.system.androidpigbank.models.entities.Category;
-import com.system.androidpigbank.models.entities.FixedTransaction;
 
 /**
  * Created by eferraz on 25/04/16.
@@ -34,15 +31,5 @@ public final class IntentRouter {
 
     public static void startCategoryList(Context context) {
         context.startActivity(new Intent(context, CategoryListActivity.class));
-    }
-
-    public static void startFixedTransactionManager(Context context, FixedTransaction model) {
-        Intent intent = new Intent(context, FixedTransactionManagerActivity.class);
-        intent.putExtra(Constants.BUNDLE_MODEL_DEFAULT, model);
-        context.startActivity(intent);
-    }
-
-    public static void startFixedTransactionList(Context context) {
-        context.startActivity(new Intent(context, FixedTransactionListActivity.class));
     }
 }

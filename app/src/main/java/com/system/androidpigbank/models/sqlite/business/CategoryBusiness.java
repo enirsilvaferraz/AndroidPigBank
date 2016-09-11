b59d9ffbf5c8f3017c91ddced6880630cf075325
Enir F Silva Ferraz <enirsilvaferraz@gmail.com>
2016-09-11 12:39:15 -0300

    [REVIEW] Refactory

--- app/src/main/java/com/system/androidpigbank/models/sqlite/business/CategoryBusiness.java
+++ app/src/main/java/com/system/androidpigbank/models/sqlite/business/CategoryBusiness.java
@@ -1,18 +1,22 @@
 package com.system.androidpigbank.models.sqlite.business;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import com.j256.ormlite.android.AndroidConnectionSource;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
+import com.system.androidpigbank.controllers.vos.TotalVO;
+import com.system.architecture.adapters.CardAdapter;
 import com.system.architecture.helpers.JavaHelper;
 import com.system.androidpigbank.models.sqlite.entities.Category;
 import com.system.androidpigbank.models.sqlite.entities.Transaction;
 import com.system.androidpigbank.models.sqlite.persistences.DaoAbs;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -92,4 +96,67 @@ public class CategoryBusiness extends DaoAbs<Category> {
 //
 //        return category;
 //    }
+
+    @NonNull
+    public List<CardAdapter.CardModel> organizeCategorySummaryList( List<Category> data ) {
+
+        List<CardAdapter.CardModel> itens = new ArrayList<>();
+
+//        boolean alreadyAddedSecoundary = false;
+        //this.itens.add(new TitleVO("Primary Categories"));
+
+        for (Category category : data) {
+
+//            if (!alreadyAddedSecoundary && !category.isPrimary()) {
+//                TitleVO titleVO = new TitleVO("Secondary Categories");
+//                titleVO.setCardStrategy(CardAdapter.CardModeItem.NO_STRATEGY);
+//                itens.add(titleVO);
+//                alreadyAddedSecoundary = true;
+//            }
+
+            if (category.getTransactionList().isEmpty()) {
+                category.setCardStrategy(CardAdapter.CardModeItem.SINGLE);
+                itens.add(category);
+            } else {
+                category.setCardStrategy(CardAdapter.CardModeItem.START);
+                itens.add(category);
+                itens.addAll(getTransactionByCategory(category));
+            }
+        }
+        return itens;
+    }
+
+    private List<CardAdapter.CardModel> getTransactionByCategory(Category category) {
+
+        String nomeCat = null;
+        Double value = 0D;
+
+        List<CardAdapter.CardModel> innerItens = new ArrayList<>();
+        for (int i = 0; i < category.getTransactionList().size(); i++) {
+
+            Transaction transaction = category.getTransactionList().get(i);
+            String name = transaction.getCategorySecondary() != null ? transaction.getCategorySecondary().getName() : "";
+
+            if (!innerItens.isEmpty() && !name.equals(nomeCat)) {
+                TotalVO totalVO = new TotalVO(value);
+                totalVO.setCardStrategy(CardAdapter.CardModeItem.MIDDLE);
+                innerItens.add(totalVO);
+                value = 0D;
+            }
+
+            nomeCat = name;
+
+            transaction.setCardStrategy(CardAdapter.CardModeItem.MIDDLE);
+            innerItens.add(transaction);
+
+            value += transaction.getValue();
+            if (i == category.getTransactionList().size() - 1) {
+                TotalVO totalVO = new TotalVO(value);
+                totalVO.setCardStrategy(CardAdapter.CardModeItem.END);
+                innerItens.add(totalVO);
+            }
+        }
+
+        return innerItens;
+    }
 }

9d8f59643cf2caa59df7b99075d5101e0b32602a
Enir Francisco Silva Ferraz <eferraz@ciandt.com>
2016-09-19 18:36:04 -0300

    [BUG] Lista de categorias desordenada

--- app/src/main/java/com/system/androidpigbank/controllers/vos/CategoryVO.java
+++ app/src/main/java/com/system/androidpigbank/controllers/vos/CategoryVO.java
@@ -14,6 +14,7 @@ import com.system.architecture.adapters.CardAdapter;
 import com.system.architecture.managers.EntityAbs;
 import com.system.architecture.utils.JavaUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -52,6 +53,8 @@ public class CategoryVO extends EntityAbs implements VOIf, Parcelable, CardAdapt
     private CategoryVO old;
 
     public CategoryVO() {
+        transactionList = new ArrayList<>();
+        amount = 0D;
     }
 
     public CategoryVO(String name, Double amount) {
--- app/src/main/java/com/system/androidpigbank/models/firebase/business/HomeBusiness.java
+++ app/src/main/java/com/system/androidpigbank/models/firebase/business/HomeBusiness.java
@@ -67,111 +67,89 @@ public class HomeBusiness {
 
     private void verifyNextStep(final int month, final int year, List<TransactionVO> transactions, List<CategoryVO> categories, List<MonthVO> months, SingleResult listener) {
         if (transactions != null && categories != null && months != null) {
-            listener.onFind(fillHomeObject(month, year, transactions, categories, months));
+            listener.onFind(fillHomeObjectV2(month, year, transactions, categories, months));
         }
     }
 
+    private HomeObjectVO fillHomeObjectV2(final int monthInt, final int yearInt, List<TransactionVO> transactions, List<CategoryVO> categories, List<MonthVO> months) {
 
-    private HomeObjectVO fillHomeObject(final int monthInt, final int yearInt, List<TransactionVO> transactions, List<CategoryVO> categories, List<MonthVO> months) {
+        final MonthVO month = new MonthVO();
+        month.setMonth(monthInt);
+        month.setYear(yearInt);
+        month.setValue(0D);
 
         final HomeObjectVO home = new HomeObjectVO();
         home.setMonth(monthInt);
         home.setYear(yearInt);
-        home.setListCategorySummary(new ArrayList<CategoryVO>());
+
+        for (TransactionVO tvo : transactions) {
+            CategoryVO cvo = categories.get(categories.indexOf(tvo.getCategory()));
+            cvo.setAmount(cvo.getAmount() + tvo.getValue());
+            tvo.setCategory(cvo);
+            cvo.getTransactionList().add(tvo);
+
+            if (tvo.getCategorySecondary() != null) {
+                CategoryVO cvo2 = categories.get(categories.indexOf(tvo.getCategorySecondary()));
+                cvo2.setAmount(cvo2.getAmount() + tvo.getValue());
+                tvo.setCategorySecondary(cvo2);
+                cvo2.getTransactionList().add(tvo);
+
+                Collections.sort(cvo2.getTransactionList(), new TransactionSort());
+            }
+
+            Collections.sort(cvo.getTransactionList(), new TransactionSort());
+            month.setValue(month.getValue() + tvo.getValue());
+        }
+
         home.setListTransaction(transactions);
-        home.setListMonth(new ArrayList<MonthVO>());
-
-        for (TransactionVO transaction : transactions) {
-            CategoryVO category = getCategoryIndex(categories, transaction, transaction.getCategory());
-            transaction.setCategory(category);
-            if (!home.getListCategorySummary().contains(category)) {
-                home.getListCategorySummary().add(category);
-            } else {
-                int index = home.getListCategorySummary().indexOf(category);
-                home.getListCategorySummary().set(index, category);
-            }
-
-            if (transaction.getCategorySecondary() != null) {
-                CategoryVO categoryS = getCategoryIndex(categories, transaction, transaction.getCategorySecondary());
-                transaction.setCategorySecondary(categoryS);
-                if (!home.getListCategorySummary().contains(categoryS)) {
-                    home.getListCategorySummary().add(categoryS);
-                } else {
-                    int index = home.getListCategorySummary().indexOf(categoryS);
-                    home.getListCategorySummary().set(index, categoryS);
-                }
-            }
-        }
-
-        Collections.sort(home.getListCategorySummary(), new Comparator<CategoryVO>() {
-            @Override
-            public int compare(CategoryVO o1, CategoryVO o2) {
-                int compare = Boolean.compare(o1.isPrimary(), o2.isPrimary());
-                return compare == 0 ? o1.getName().compareTo(o2.getName()) : compare;
-            }
-        });
-
-
-        Double total = 0D;
-        for (CategoryVO category : home.getListCategorySummary()) {
-            total += category.getAmount();
-
-            Collections.sort(category.getTransactionList(), new Comparator<TransactionVO>() {
-                @Override
-                public int compare(TransactionVO o1, TransactionVO o2) {
-
-                    if (o1.getCategorySecondary() == null){
-                        return -1;
-                    } else if (o2.getCategorySecondary() == null) {
-                        return 1;
-                    } else {
-                        return o1.getCategorySecondary().getName().compareTo(o2.getCategorySecondary().getName());
-                    }
-                }
-            });
-        }
-
-        MonthVO month = new MonthVO();
-        month.setMonth(monthInt);
-        month.setYear(yearInt);
+        home.setListCategorySummary(categories);
 
         if (!months.contains(month)) {
-            month.setValue(total);
             months.add(new MonthFirebaseBusiness().save(month));
         } else {
             int index = months.indexOf(month);
             MonthVO monthVO = months.get(index);
-            if (!monthVO.getValue().equals(total)) {
-                monthVO.setValue(total);
-                new MonthFirebaseBusiness().save(monthVO);
-                months.set(index, monthVO);
+            if (!monthVO.getValue().equals(month.getValue())) {
+                monthVO.setValue(month.getValue());
+                months.set(index, new MonthFirebaseBusiness().save(monthVO));
             }
         }
 
+        Collections.sort(months, new MonthSort());
+
         home.setListMonth(months);
-
-        Collections.sort(months, new Comparator<MonthVO>() {
-            @Override
-            public int compare(MonthVO o1, MonthVO o2) {
-                int compareToYear = o1.getYear().compareTo(o2.getYear());
-                return compareToYear != 0 ? compareToYear : o1.getMonth().compareTo(o2.getMonth());
-            }
-        });
-
         return home;
     }
 
-    private CategoryVO getCategoryIndex(List<CategoryVO> categories, TransactionVO transaction, CategoryVO categoryParam) {
+    private class MonthSort implements Comparator<MonthVO> {
 
-        CategoryVO category = categories.get(categories.indexOf(categoryParam));
-        category.setAmount((category.getAmount() != null ? category.getAmount() : 0) + transaction.getValue());
-
-        if (category.getTransactionList() == null) {
-            category.setTransactionList(new ArrayList<TransactionVO>());
+        @Override
+        public int compare(MonthVO o1, MonthVO o2) {
+            int compareToYear = o1.getYear().compareTo(o2.getYear());
+            return compareToYear != 0 ? compareToYear : o1.getMonth().compareTo(o2.getMonth());
         }
+    }
 
-        category.getTransactionList().add(transaction);
-        return category;
+    private class TransactionSort implements Comparator<TransactionVO> {
+
+        @Override
+        public int compare(TransactionVO o1, TransactionVO o2) {
+            if (o1.getCategorySecondary() == null) {
+                return -1;
+            } else if (o2.getCategorySecondary() == null) {
+                return 1;
+            } else {
+                return o1.getCategorySecondary().getName().compareTo(o2.getCategorySecondary().getName());
+            }
+        }
+    }
+
+    private class CategorySort implements Comparator<CategoryVO> {
+        @Override
+        public int compare(CategoryVO o1, CategoryVO o2) {
+            int compare = Boolean.compare(o1.isPrimary(), o2.isPrimary());
+            return compare != 0 ? compare : o1.getName().compareTo(o2.getName());
+        }
     }
 
     /**

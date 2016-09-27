7cf1acf2958ad8215ae40886d4f47fc068f1ae8d
Enir F Silva Ferraz <enirsilvaferraz@gmail.com>
2016-09-27 20:40:13 -0300

    [IMPL] Ajustes para criacao do custom view de percentual progress

--- app/src/main/java/com/system/androidpigbank/views/CustomPercentualProgress.java
+++ app/src/main/java/com/system/androidpigbank/views/CustomPercentualProgress.java
@@ -1,7 +1,9 @@
 package com.system.androidpigbank.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -27,6 +29,13 @@ public class CustomPercentualProgress extends LinearLayout {
         super(context, attrs);
         inflate(getContext(), R.layout.custom_percentual_progress, this);
         ButterKnife.bind(this);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomPercentualProgress, 0, 0);
+        try {
+            mValue.setTextSize(TypedValue.COMPLEX_UNIT_PX, a.getDimensionPixelSize(R.styleable.CustomPercentualProgress_percentTextSize, 16));
+        } finally {
+            a.recycle();
+        }
     }
 
     public void bind(Double maxValue, Double totalValue) {
@@ -34,6 +43,6 @@ public class CustomPercentualProgress extends LinearLayout {
         Integer percentual = Double.valueOf(totalValue * 100 / (maxValue == 0 ? 1 : maxValue)).intValue();
 
         mProgress.setProgress(percentual);
-        mValue.setText(String.format("d%", percentual));
+        mValue.setText(percentual + "%");
     }
 }

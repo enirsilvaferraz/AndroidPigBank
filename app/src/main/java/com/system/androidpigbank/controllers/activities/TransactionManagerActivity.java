7660732ee59cfc82508230f33da0c27cf45515e2
Enir Francisco Silva Ferraz <eferraz@ciandt.com>
2017-02-27 15:07:12 -0300

    [IMPL] Nova tela de cadastro de lancamentos (ideia V3)

--- app/src/main/java/com/system/androidpigbank/controllers/activities/TransactionManagerActivity.java
+++ app/src/main/java/com/system/androidpigbank/controllers/activities/TransactionManagerActivity.java
@@ -20,25 +20,8 @@ public class TransactionManagerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_content_transaction_manager);
+        setContentView(R.layout.new_content_transaction_manager_v3);
         ButterKnife.bind(this);
-
-        String[] values = new String[]{"Android List View",
-                "Adapter implementation",
-                "Simple List View In Android",
-                "Simple List View In Android",
-                "Simple List View In Android",
-                "Simple List View In Android",
-                "Simple List View In Android",
-                "Simple List View In Android",
-                "Simple List View In Android",
-                "Simple List View In Android",
-                "Create List View Android",
-                "Android Example",
-                "List View Source Code",
-                "List View Array Adapter",
-                "Android Example List View"
-        };
 
 
     }
---
+++ app/src/main/java/com/system/androidpigbank/controllers/adapters/TransactionManagerPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.system.androidpigbank.controllers.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+
+/**
+ * Created by eferraz on 03/02/17.
+ */
+
+public class TransactionManagerPagerAdapter extends FragmentPagerAdapter {
+
+    public TransactionManagerPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
